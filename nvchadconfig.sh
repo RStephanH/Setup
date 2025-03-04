409d3b86eb15c3b8adac26e36e056faae0cdb508
rsh <randrianirinahernandezstephan@gmail.com>
2025-03-05 00:45:12 +0300

    lsp server added

--- nvchadconfig.sh
+++ nvchadconfig.sh
@@ -6,6 +6,13 @@ if [ echo $? -eq 0 ] ; then
     cd Setup
     mv ~/.config/nvim/init.lua init.lua.bak
     mv init.lua ~/.config/nvim/
+    if command npm -v &> /dev/null; then
+        npm install -g pyright
+        npm install -g html-language-bin
+    fi
+    if command pacman -v &> /dev/null; then
+        sudo pacman -S clangd
+    fi
 else
     echo "Failed to clone  the repository"
     exit 1
