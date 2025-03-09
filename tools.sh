82c3ad7f987646f22322df9340cdce99705f707b
rsh <randrianirinahernandezstephan@gmail.com>
2025-03-09 18:56:12 +0300

    Func to detect ctrl-c --not-tested--

--- tools.sh
+++ tools.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/bash
-tools=("ghostty","remmina","obs-studio","libvirtd","quemu","yakuake","wireshark","obsidian","telegram-desktop","thinderbird","virtual-box","cmake","kernel-devel")
-for tool in "${tools[@]}"; do
-    sudo pacman -S $tool
-    if [ echo $? -eq 0 ];then
-        echo $tool installed
-    else
-        echo "$tool not installed"
-    fi
-done
+cleanup() {
+  echo "Script aborted!"
+  exit 1
+}
+trap cleanup INT {
+  tools=("ghostty" "remmina" "obs-studio" "libvirtd" "qemu" "yakuake" "wireshark" "obsidian" "telegram-desktop" "thinderbird" "virtual-box" "cmake" "kernel-devel")
+  for tool in "${tools[@]}"; do
+      sudo pacman -S $tool
+      if [ echo $? -eq 0 ];then
+          echo $tool installed
+      else
+          echo "$tool not installed"
+      fi
+  done
+}
