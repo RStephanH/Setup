216bc2b1f364fb91627d367f668f6eeb0066c543
rsh <randrianirinahernandezstephan@gmail.com>
2025-03-12 20:50:57 +0300

    List of packages updated

--- yaypackage.sh
+++ yaypackage.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
-tools=("zapzap","onlyoffice-bin")
+tools=("zapzap" "onlyoffice-bin" "zen-browser-bin")
 for tool in ${tools[@]}; do
+    echo "$tool"
     yay -Suy $tool --no-confirm
+
 done
