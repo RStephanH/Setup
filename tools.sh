da43751eefdbd09d249f17f28b871d667dca76dc
rsh <randrianirinahernandezstephan@gmail.com>
2025-03-20 23:19:52 +0300

    refac: list of tools updated and if statement corrected

--- tools.sh
+++ tools.sh
@@ -3,14 +3,14 @@ cleanup() {
   echo "Script aborted!"
   exit 1
 }
-trap cleanup INT {
-  tools=("ghostty" "remmina" "obs-studio" "libvirtd" "qemu" "yakuake" "wireshark" "obsidian" "telegram-desktop" "thinderbird" "virtual-box" "cmake" "kernel-devel")
+trap cleanup INT 
+  tools=("ghostty" "remmina" "obs-studio" "libvirtd" "qemu" "yakuake" "wireshark" "obsidian" "telegram-desktop" "thinderbird" "virtualbox" "cmake" "kernel-devel")
   for tool in "${tools[@]}"; do
       sudo pacman -S $tool
-      if [ echo $? -eq 0 ];then
-          echo $tool installed
+      if [ $? -eq 0 ]; then
+          echo "$tool installed"
       else
           echo "$tool not installed"
       fi
   done
-}
+
