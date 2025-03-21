af40fcf49245f0399131a711562a64fd70084d52
rsh <randrianirinahernandezstephan@gmail.com>
2025-03-21 23:19:55 +0300

    refac :correction the code syntaxe

--- gns3.sh
+++ gns3.sh
@@ -1,10 +1,18 @@
 #/bin/bash
+cleanup() {
+  echo "Script aborted"
+  exit 1
+}
+trap cleanup INT
 dependecies=("qemu docker vpcs dynamips libvirt ubridge inetutils")
 gns3=("gns3-server gns3-gui")
 echo "let's install the dependecies first"
-for dependecie in $dependecies; do
+for dependecie in "${dependecies[@]}"; do
   echo "Package=$dependecie"
-  yay -S $dependecie
+  yay -S "$dependecie"
+  if [$? -eq 0]; then
+    echo "$dependecie installed."
+  fi
 done
 echo "All dependecies are installed"
 for packet in $gns3; do
