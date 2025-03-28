12c69be74aa115bd5bb854d711fdb11e0812aa98
rsh <randrianirinahernandezstephan@gmail.com>
2025-03-28 22:52:34 +0300

    feat : varia and brave added

--- yaypackage.sh
+++ yaypackage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-tools=("zapzap" "onlyoffice-bin" "zen-browser-bin")
+tools=("zapzap" "onlyoffice-bin" "zen-browser-bin" "varia" "brave-browser-bin")
 for tool in ${tools[@]}; do
     echo "$tool"
     yay -Suy $tool --no-confirm
