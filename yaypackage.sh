2e5838c2f4c1c7cb89e9133587f46035521bbded
rsh <randrianirinahernandezstephan@gmail.com>
2025-04-01 13:31:56 +0300

    fix: typo

--- yaypackage.sh
+++ yaypackage.sh
@@ -2,6 +2,6 @@
 tools=("zapzap" "onlyoffice-bin" "zen-browser-bin" "varia" "brave-browser-bin")
 for tool in ${tools[@]}; do
     echo "$tool"
-    yay -Suy $tool --no-confirm
+    yay -Suy $tool --noconfirm
 
 done
